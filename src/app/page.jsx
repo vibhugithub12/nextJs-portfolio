1d653dcfca09b1525cbde21d899ff3675a2d2cf4
Vaibhav <vibhuchauhan37@gmail.com>
2024-07-17 08:43:05 +0530

    Transition with framer-motion for hamburger menu given

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,24 @@
+import Image from "next/image";
 const Homepage = () => {
-  return <div>Hello World</div>;
+  return <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
+    {/* Image container */}
+    <div className="h-1/2 relative lg:h-full lg:w-1/2">
+      <Image src="/hero.png" alt="myImg" fill className="object-contain" />
+    </div>
+
+    {/* Text container */}
+    <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-8 items-center justify-center">
+      {/* Title */}
+      <h1 className="text-4xl font-bold md:text-6xl">Your Vision, My Code </h1>
+      {/* Ḍescription */}
+      <p className="md:text-xl">Turning your dream website into a reality with the latest techonology and precision.</p>
+      {/* Buttons */}
+      <div className="flex w-full gap-4">
+        <button className="p-4 rounded-lg ring-1 ring-black bg-black text-white">View my work</button>
+        <button className="p-4 rounded-lg ring-1 ring-black ">Contact me</button>
+      </div>
+    </div>
+  </div>;
 };
 
 export default Homepage;
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,7 +2,8 @@
 
 import Link from "next/link";
 import { useState } from "react";
-import NavLink from './navlink'
+import NavLink from './navlink';
+import {motion} from 'framer-motion';
 
 const MenuList=[
     {url:"/",title:"Home"},
@@ -14,6 +15,61 @@ const MenuList=[
 const Navbar=()=> {
 
     const [open,setOpen]=useState(false)
+
+    // Animation for hamburger menu
+    const topVariants={
+        closed:{
+            rotate:0,
+        },
+        opened:{
+            rotate:45,
+            backgroundColor:"rgb(255,255,255)"
+        }
+    }
+
+    const centerVariants={
+        closed:{
+            opacity:1,
+        },
+        opened:{
+            opacity:0,
+        }
+    }
+
+    const bottomVariants={
+        closed:{
+            rotate:0,
+        },
+        opened:{
+            rotate:-45,
+            backgroundColor:"rgb(255,255,255)"
+        }
+    }
+
+    // Animation for navigation links after opening hamburger menu
+    const listVariants={
+        closed:{
+            x:"100vw"
+        },
+        opened:{
+            x:"0",
+            transition:{
+                when:"beforeChildren",
+                staggerChildren:0.2
+            }
+        }
+    }
+
+    const listItemVariants={
+        closed:{
+            x:-10,
+            opacity:0,
+        },
+        opened:{
+            x:0,
+            opacity:1
+        }
+    }
 
     return (
         <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 text-xl">
@@ -52,19 +108,28 @@ const Navbar=()=> {
             <div className="md:hidden">
                 {/* menu button */}
                 <button onClick={()=>setOpen((prev)=>!prev)} className="w-10 h-8 flex flex-col justify-between z-50 relative">
-                    <div className="w-10 h-1 bg-white rounded "></div>
-                    <div className="w-10 h-1 bg-white rounded "></div>
-                    <div className="w-10 h-1 bg-white rounded "></div>
+                    <motion.div variants={topVariants}
+                      animate={open?"opened":"closed"}
+                     className="w-10 h-1 bg-black rounded origin-left"></motion.div>
+                    <motion.div variants={centerVariants}
+                      animate={open?"opened":"closed"}
+                     className="w-10 h-1 bg-black rounded "></motion.div>
+                    <motion.div variants={bottomVariants}
+                      animate={open?"opened":"closed"}
+                     className="w-10 h-1 bg-black rounded origin-left"></motion.div>
                 </button>
                 {/* menu list */}
                 {open &&(
-                <div className="absolute top-0 left-0 w-screen h-screen bg-black text-white flex flex-col items-center justify-center gap-8 text-4xl">
+                <motion.div variants={listVariants} initial="closed" animate="opened" className="absolute top-0 left-0 w-screen h-screen bg-black text-white flex flex-col items-center justify-center gap-8 text-4xl z-40">
                     {MenuList.map(item=>(
-                        <Link href={item.url} key={item.title}>
+                        <motion.div variants={listItemVariants} className="" key={item.title}>
+                        <Link href={item.url}>
                             {item.title}
                         </Link>
+                    </motion.div>
                     ))}
-                    </div>
+
+                    </motion.div>
                  )}
             </div>
         </div>
